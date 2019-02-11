c39bb6bb2ea1db8bd465dc35222ec720bda863a5
Jacob McSwain <jacob@clevyr.com>
2019-02-10 18:02:26 -0600

    Initial Release 0.0.1

---
+++ README.md
@@ -0,0 +1,37 @@
+# slackpresence README
+
+Sets your Slack status to the file name and language you're working in
+
+## Features
+
+* Sends language and filename to your Slack workspace.
+* More to come
+
+## Requirements
+
+My Slack [app](https://slack.com/oauth/authorize?client_id=5167321442.546836577892&scope=users.profile:write) is required, you can add it through the popup when you install this extension.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `slackpresence.enabled`: enable/disable this extension
+* `slackpresence.authToken`: the auth token for Slack API calls, you usually shouldn't touch this
+
+## Known Issues
+
+* There are few features, I'm still learning VSCode Extensions
+
+## Release Notes
+
+### 0.0.1
+
+Initial release of slackpresence
+
+-----------------------------------------------------------------------------------------------------------
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,160 @@
+const vscode = require('vscode');
+const basename = require('path').basename;
+const https = require('https');
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring')
+
+var config = vscode.workspace.getConfiguration('slackpresence');
+
+const statusBarIcon = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+statusBarIcon.text = '$(pulse) Connecting to Slack...';
+
+var slackTimer;
+var lastFile;
+
+const server = http.createServer(function (req, res) {
+	const closeTab = "<h2>Logged In</h2><h4>You may close this window</h4>"
+	res.end(closeTab);
+	server.close();
+	var query = url.parse(req.url, true).query;
+	sendRequest('/api/oauth.access', {
+		client_id: '5167321442.546836577892',
+		client_secret: 'bb787469ad4ce3ad53415a0e05bc288d',
+		code: query.code,
+		redirect_uri: 'http://localhost:8989'
+	}, false, (res) => {
+		var body = '';
+
+		res.on('data', (chunk) => {
+			body += chunk;
+		});
+		res.on('end', () => {
+			var response = JSON.parse(body);
+			if (!response.error) {
+				config.update('authToken', response.access_token, true).then(() => {
+					// For some reason the config doesn't reload on its own
+					config = vscode.workspace.getConfiguration('slackpresence');
+					startSharing();
+				});
+			} else {
+				console.log(response.error);
+			}
+		});
+	});
+});
+
+function createOauthListener() {
+	server.listen(8989);
+	console.log("OAUTH server is listening");
+}
+
+function sendRequest(path, data, dataIsJson = true, callback = null) {
+	const postData = dataIsJson ? JSON.stringify(data) : querystring.stringify(data);
+	const options = {
+		hostname: 'slack.com',
+		port: 443,
+		path: path,
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${config.get('authToken')}`,
+			'Content-Type': dataIsJson ? 'application/json; charset=utf-8' : 'application/x-www-form-urlencoded',
+			'Content-Length': postData.length
+		}
+	};
+	var req = https.request(options, callback);
+	req.on('error', (e) => {
+		console.error(e);
+	});
+
+	req.write(postData);
+	req.end();
+	return req;
+}
+
+function sendActivity() {
+	if (vscode.window.activeTextEditor) {
+		const fileName = basename(vscode.window.activeTextEditor.document.fileName);
+		if (fileName != lastFile) {
+			lastFile = fileName;
+			const language = vscode.window.activeTextEditor.document.languageId;
+			console.log(`Working on ${language}: ${fileName}`);
+			const postData = {
+				profile: {
+					status_text: `Working on ${language}: ${fileName}`,
+					status_emoji: ":vscode:",
+					status_expiration: 0
+				}
+			};
+			sendRequest('/api/users.profile.set', postData, true, (res) => {
+				var body = '';
+		
+				res.on('data', (chunk) => {
+					body += chunk;
+				});
+				res.on('end', () => {
+					var response = JSON.parse(body);
+					if (response.error) {
+						console.log(response.error);
+					}
+				});
+			});
+		}
+	}
+}
+
+function startSharing() {
+	statusBarIcon.text = '$(pulse) Sharing with Slack...';
+	sendActivity();
+	slackTimer = setInterval(() => {
+		sendActivity();
+	}, 200);
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	console.log('slackpresence is active');
+
+	if (config.get('enabled')) {
+		statusBarIcon.show();
+		if (!config.get('authToken')) {
+			vscode.window.showErrorMessage('You must install the Slack Integration into your workspace\n[Add to Slack](https://slack.com/oauth/authorize?client_id=5167321442.546836577892&scope=users.profile:write)');
+			createOauthListener();
+			statusBarIcon.text = '$(pulse) Error connecting to Slack...';
+		} else {
+			startSharing();
+		}
+	}
+
+	const enabler = vscode.commands.registerCommand('slackpresence.enable', () => {
+		config.update('enabled', true);
+		statusBarIcon.show();
+		vscode.window.showInformationMessage('Enabled Slack Presence for this workspace.');
+	});
+
+	const disabler = vscode.commands.registerCommand('slackpresence.disable', () => {
+		config.update('enabled', false);
+		statusBarIcon.hide();
+		vscode.window.showInformationMessage('Disabled Slack Presence for this workspace.');
+	});
+
+	context.subscriptions.push(enabler, disabler)
+}
+
+function deactivate() {
+	clearImmediate(slackTimer);
+	const postData = {
+		profile: {
+			status_text: '',
+			status_emoji: ''
+		}
+	};
+	return sendRequest('/api/users.profile.set', postData);
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
